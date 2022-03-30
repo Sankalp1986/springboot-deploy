46952df0d719a19cb48fadcfa16840a5b0c663db
Sankalp1986 <50266465+Sankalp1986@users.noreply.github.com>
2022-03-30 13:02:07 +0530

    added log files

--- src/main/java/com/test/springboot/SpringbootDeployApplication.java
+++ src/main/java/com/test/springboot/SpringbootDeployApplication.java
@@ -4,17 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @RestController
 public class SpringbootDeployApplication {
-
+ Logger logger = LoggerFactory.getLogger(SpringbootDeployApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDeployApplication.class, args);
 	}
    
 	@GetMapping("/message")
 	public String message() {
+		logger.info("*****Insdie Message method*****");
 		return "Hi deployed successfully";
 	}
 }
